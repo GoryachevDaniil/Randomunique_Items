7d7d4feef4c017ea9427d7a42b84d67bbea276ed
Daniil <81791927+GoryachevDaniil@users.noreply.github.com>
2021-09-08 05:07:41 +0300

    add random range includes

--- Randomunique_Items.py
+++ Randomunique_Items.py
@@ -1,44 +1,20 @@
 from random import randint
 
-    ## ## Make list  to take elements from. ## ## 
 
-    
-print('Enter the list items on one line separated by a space:', end=' ')
-input_list = [m for m in input().split()]
+print('Enter the list items on one line separated by a space:', end=' ')            
+input_list = [m for m in input().split()]                                           
 N = len(input_list)
-
-
-    ## ## Enter len of final list. ## ## 
 
 print('Enter the number of elements to sample:', end=' ')
 k = int(input())
 
-
-    ## ## Prepear to accounting. ## ##
-    
-if 0 <= k <= N:                         # check added
-    output_list = []                    # list to display elements
-    index_list = []                     # list for accounting added indexes
-    steps = 0                           # cicle counter
-
-    
-    ## ## Cicle ending only after founding entered number of elements. ## ## 
-        
-    while len(output_list) != k:
-        index = randint(0, N - 1)
-        steps += 1                      #scored how much cicles done to find all
-        if index not in index_list:
-            index_list.append(index)
-            output_list.append(input_list[index])
-          
-        
-    ## ## Print result of a programm. ## ##
-            
-    print('List:', output_list, 'make in', steps, 'steps')
-    print("algorithm complexity: O(N**2) =", N**2)
-    
-    
-    ## ## If entered number is more then len of input_list ## ##
-    
+if 0 <= k <= N:                                                                     
+    index_list = [i for i in range(0, N)]                                           
+    output_list = []
+    while N != k + len(index_list):
+        index = randint(0, len(index_list) - 1)
+        output_list.append(index_list.pop(index))
+    print('List:', output_list)
+    print("algorithm complexity: O(k) =", k)
 else:
-    print('Invalid input. Try one more times')
+    print('Invalid input. Try one more times.')
