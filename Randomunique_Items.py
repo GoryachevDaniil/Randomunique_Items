777854fa3104af3e4e5533be8457410493ddd9fd
Daniil <81791927+GoryachevDaniil@users.noreply.github.com>
2021-09-09 07:31:28 +0300

    a

--- Randomunique_Items.py
+++ Randomunique_Items.py
@@ -1,20 +1,21 @@
 from random import randint
 
 
-print('Enter the list items on one line separated by a space:', end=' ')            
-input_list = [m for m in input().split()]                                           
+print('Enter the list items on one line separated by a space:', end=' ')
+input_list = [m for m in input().split()]
 N = len(input_list)
 
 print('Enter the number of elements to sample:', end=' ')
 k = int(input())
 
-if 0 <= k <= N:                                                                     
-    index_list = [i for i in range(0, N)]                                           
+if 0 <= k <= N:
+    index_list = [i for i in range(0, N)]
     output_list = []
     while N != k + len(index_list):
         index = randint(0, len(index_list) - 1)
-        output_list.append(index_list.pop(index))
+        output_list.append(input_list[index_list[index]])
+        index_list.pop(index)
     print('List:', output_list)
-    print("algorithm complexity: O(k) =", k)
+    print("Algorithm complexity: O(k) =", k)
 else:
     print('Invalid input. Try one more times.')
